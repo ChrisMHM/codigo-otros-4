07cc8606abec82b8e27ea07276a7eebf3cd5d6c4
Christian Hernández <christian.mhm@outlook.com>
2024-02-08 12:52:43 -0600

    Corrección de error

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -35,7 +35,7 @@ public class Ejercicio4 {
           }
           break;
       }
-      System.out.println("Gana el jugador " + g);
+      System.out.println("Gana el jugador " + ganador);
     }
   }
 }
